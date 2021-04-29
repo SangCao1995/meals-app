06e51351b9e3d965e171808b9d7cf52b61afe91d
Sangcao1995 <caobathanhsang@gmail.com>
2021-04-29 21:52:15 +0700

    Improve Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   title: {
-    fontWeight: 'bold',
+    fontFamily: 'OpenSans-Bold',
     color: Platform.OS === 'android' ? 'white' : Colors.primary,
     fontSize: 20,
   },
